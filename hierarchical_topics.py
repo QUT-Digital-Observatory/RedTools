cd3e3732dc8a17eb91a42bf06fa76732292a9f0c
deimosnz <rfle001@gmail.com>
2024-07-22 14:58:49 +1000

    code added

--- hierarchical_topics.py
+++ hierarchical_topics.py
@@ -15,7 +15,6 @@ import plotly.graph_objects as go
 from tqdm.auto import tqdm
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
-from RedTools import topic_window
 from topic_window import TopicWindow
 
 
@@ -27,7 +26,7 @@ def load_config(config_path: str) -> dict:
         config = yaml.safe_load(file)
     return config
 
-config_path = 'config.yaml'
+config_path = '/home/fleetr/RedTools/config.yaml'
 
 config = load_config(config_path)
 hardware = config.get('hardware', 'GPU')
@@ -50,13 +49,37 @@ class HierarchicalTopics:
         with open(config_path, 'r') as file:
             self.config = yaml.safe_load(file)
 
-    def get_hierarchical_topics(self, data, text_column, date_column, timescale, window_size):
+    def get_hierarchical_topics(self, data, text_column, date_column, timescale):
 
         hierarchical_topics = []
+        topic_trees = []
+        figs = []
         data_exp = self.topic_windows.expand_dataframe_with_sentences(data, text_column)
         frames = self.topic_windows.get_frames(data_exp, date_column, timescale)
-        for frame in frames:
+        for frame in tqdm(frames):
             model = self.bert_topic.fit(frame[text_column])
             docs: list = frame[text_column].tolist()
-            hierarchical_topics =model.hierarchical_topics(docs)
+            h_tops =model.hierarchical_topics(docs)
+            hierarchical_topics.append(h_tops)
+            t_trees = model.get_topic_tree(h_tops)
+            topic_trees.append(t_trees)
+            fig = model.visualize_hierarchy()
+            figs.append(fig)
+        return hierarchical_topics, topic_trees, figs    
+
+    def get_topic_embeddings(self, data, text_column, date_column, timescale):
+        topic_embeddings = []
+        data_exp = self.topic_windows.expand_dataframe_with_sentences(data, text_column)
+        frames = self.topic_windows.get_frames(data_exp, date_column, timescale)
+        for frame in tqdm(frames):
+            model = self.bert_topic.fit(frame[text_column])
+            all_topics = sorted(list(model.get_topics().keys()))
+            freq_df = model.get_topic_freq()
+            freq_df = freq_df.loc[freq_df.Topic != -1, :]
+            topics = freq_df.Topic.tolist()
+            indices = np.array([all_topics.index(topic) for topic in topics])
+            embeddings = model.topic_embeddings_[indices]
+            topic_embeddings.append(np.array(embeddings))
+        return topic_embeddings
+
 
