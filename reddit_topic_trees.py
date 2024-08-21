8bf01974bc6c0770ac33f6afde722671167042c0
deimosnz <rfle001@gmail.com>
2024-08-21 16:57:31 +1000

    Some stuff

--- reddit_topic_trees.py
+++ reddit_topic_trees.py
@@ -392,6 +392,49 @@ class Reddit_trees:
         adj_list_df_tree = pd.DataFrame(adj_data)
     
         return G_tree, adj_list_df_tree
+    
+    def tree_graph_and_adj_list_nt(self, df: pd.DataFrame, incl_topic: bool = True, topic_column = 'Topic') -> Tuple[nx.DiGraph, pd.DataFrame]:
+        # Create directed graph
+        G_tree = nx.DiGraph()
+
+        # Add nodes to the graph
+        for index, row in df.iterrows():
+            node_id = row['commentId']
+            author = row['username']
+            body = row['text']
+            link_id = row['threadId']
+            time_created = row['date']
+            node_data = {
+                'author': author,
+                'body': body,
+                'link_id': link_id,
+                'time_created': time_created
+            }
+            if incl_topic:
+                node_data['topic'] = row[topic_column]
+            
+            G_tree.add_node(node_id, **node_data)
+
+        # Add edges to the graph and build adjacency list data
+        adj_data = {'Source': [], 'Target': [], 'TimeCreated': [], 'LinkID': []}
+        for index, row in df.iterrows():
+            parent_id = str(row['responseTo']) if pd.notna(row['responseTo']) else ''
+            source = parent_id.replace('t3_', '').replace('t1_', '')
+            targets = row['commentId']
+            time_created = row['date']
+            link_id = row['threadId']
+
+            if source and targets in G_tree.nodes:
+                G_tree.add_edge(source, targets, time_created=time_created, link_id=link_id)
+                adj_data['Source'].append(source)
+                adj_data['Target'].append(targets)
+                adj_data['TimeCreated'].append(time_created)
+                adj_data['LinkID'].append(link_id)
+
+        # Create adjacency list DataFrame
+        adj_list_df_tree = pd.DataFrame(adj_data)
+    
+        return G_tree, adj_list_df_tree
 
     def plot_basic_graph(self, G: nx.DiGraph, title: str):
         # Plot the graph
